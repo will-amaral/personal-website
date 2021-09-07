caa44e4b81bad9cd453a44043706714b1b638108
Will Amaral <will@amaral.dev>
2021-09-07 15:07:36 -0300

    [content]: Update current projects

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -2,24 +2,30 @@ import { MainLayout } from '@/layouts';
 
 function Work() {
   return (
-    <MainLayout title='My career and current projects'>
+    <MainLayout title="My career and current projects">
       <h1>Career</h1>
       <p>
-        Working with the web since 2013 and with React since 2016, I have a bachelor's
-        degree in Journalism and a B.Tech degree in Software Development. <br />I have
-        worked for small to medium size companies, building websites and web applications
-        of varied purposes. Even though I'm mostly focused in the frontend, I actually
-        consider myself a Fullstack developer.
+        Working with the web since 2013 and with React since 2016, I have a
+        bachelor's degree in Journalism and a B.Tech degree in Software
+        Development. <br />I have worked for small to medium size companies,
+        building websites and web applications of varied purposes. Even though
+        I'm mostly focused in the frontend, I actually consider myself a
+        Fullstack developer.
       </p>
       <p>I also write random stuff if you are interested.</p>
       <h2>Current Projects</h2>
       <ul>
         <li>
-          Team Lead at <a href='https://triventoeducacao.com.br/'>Trivento Educação</a>
+          Team Lead at{' '}
+          <a href="https://triventoeducacao.com.br/">Trivento Educação</a>
+        </li>
+        <li>
+          Front-end Instructor at{' '}
+          <a href="https://cubos.academy/">Cubos Academy</a>
         </li>
       </ul>
       <h2>Languages and Frameworks</h2>
-      <ul className='grid-container halves u-align-left'>
+      <ul className="grid-container halves u-align-left">
         <li>JavaScript</li>
         <li>ReactJs and cousins</li>
         <li>NodeJs</li>
